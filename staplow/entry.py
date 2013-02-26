f0bc053b95a7f4d91f1f40b04e95ff44e504fdd5
Corry Haines <tabletcorry@gmail.com>
2013-02-25 21:24:15 -0800

    Work in progress

--- staplow/entry.py
+++ staplow/entry.py
@@ -1,36 +1,63 @@
 """Staplow!
 
 Usage:
-  staplow init
-  staplow add (input|output) <port>
+  staplow init [options]
+  staplow add (input|output) <port> [options]
+  staplow delete [options]
+  staplow flush [options]
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
-  -i <interface>, --interface <interface> Specify interface
+  -i <interface>, --interface <interface> Specify interface.
   -s <source>, --source <source>
   -d <destination>, --destination <destination>
+  -f <conf>, --config <conf> [default: conf.yml]
 
 """
 from docopt import docopt
 from sh import iptables
+from operator import eq
+iptables.bake("-t" "mangle")
 
+interfaces = ["eth0",
+              "eth1"]
 
 def console():
     args = docopt(__doc__, version='Staplow 0.1')
 
+    print args
+    global interfaces
+    if args['--interface']:
+        interfaces = filter(lambda x: x == args['--interface'], interfaces)
+        print interfaces
     if args['init']:
         init(args)
     if args['add']:
         add(args)
-    
+    if args['flush']:
+        flush(args)
+    if args['delete']:
+        delete(args)
 
 def init(args):
-    iptables.bake("-t", "mangle")
-    chains_to_add = ("staplow_in", "staplow_out")
-    for chains in chains_to_add:
-        iptables("-N", chains)
+    for interface in interfaces:
+        print iptables("-N", "{0}_{1}_{2}".format("staplow", "in", interface))
+        print iptables("-N", "{0}_{1}_{2}".format("staplow", "out", interface))
+
+
+def delete(args):
+    for interface in interfaces:
+        iptables("-D", "INPUT", "-i", interface, "-j", "{0}_{1}_{2}".format("staplow", "in", interface))
+        iptables("-D", "OUTPUT", "-o", interface, "-j", "{0}_{1}_{2}".format("staplow", "out", interface))
+
+
+def flush(args):
+    for interface in interfaces:
+        iptables("-F", "{0}_{1}_{2}".format("staplow", "in", interface))
+        iptables("-F", "{0}_{1}_{2}".format("staplow", "out", interface))
 
 
 def add(args):
+
     pass
